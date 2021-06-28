00722e8ac39ed4e198e6e04a04f5c983ced398aa
Camila Suzuki <camila.suzuki.p@gmail.com>
2021-06-28 15:14:30 -0300

    Score and snake changes colors

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -49,11 +49,6 @@ public class GamePanel extends JPanel implements ActionListener {
 	
 	public void draw(Graphics g) {
 		if (running) {
-			for (int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++) {
-				g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
-				g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH,i*UNIT_SIZE);
-				
-			}
 			g.setColor(Color.red);
 			g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 			
@@ -63,10 +58,16 @@ public class GamePanel extends JPanel implements ActionListener {
 					g.fillRect(x[i],y[i], UNIT_SIZE, UNIT_SIZE);
 				} else {
 					g.setColor(new Color(45,180,0));
+					g.setColor(new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255)));
 					g.fillRect(x[i],y[i], UNIT_SIZE, UNIT_SIZE);
 				}
 				
 			}
+			g.setColor(Color.red);
+			g.setFont(new Font ("Ink Free",Font.BOLD,40));
+			FontMetrics metrics = getFontMetrics(g.getFont());
+			g.drawString("Score: "+applesEaten,(SCREEN_WIDTH - metrics.stringWidth("Score: "+applesEaten))/2,g.getFont().getSize());
+			
 		} else {
 			gameOver(g);
 		}
@@ -144,11 +145,16 @@ public class GamePanel extends JPanel implements ActionListener {
 		
 	}
 	public void gameOver(Graphics g) {
+		//Score
+		g.setColor(Color.red);
+		g.setFont(new Font ("Ink Free",Font.BOLD,40));
+		FontMetrics metrics1 = getFontMetrics(g.getFont());
+		g.drawString("Score: "+applesEaten,(SCREEN_WIDTH - metrics1.stringWidth("Score: "+applesEaten))/2,g.getFont().getSize());
 		//Game Over text
 		g.setColor(Color.red);
 		g.setFont(new Font ("Ink Free",Font.BOLD,75));
-		FontMetrics metrics = getFontMetrics(g.getFont());
-		g.drawString("Game Over",(SCREEN_WIDTH - metrics.stringWidth("Game Over"))/2,SCREEN_HEIGHT/2);
+		FontMetrics metrics2 = getFontMetrics(g.getFont());
+		g.drawString("Game Over",(SCREEN_WIDTH - metrics2.stringWidth("Game Over"))/2,SCREEN_HEIGHT/2);
 		
 	}
 
